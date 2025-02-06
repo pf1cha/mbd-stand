5b0bfc7c2f9b9b6836686ef0078dfc8218097d01
Max <lonishin.max@gmail.com>
2025-02-06 19:57:53 +0300

    added a realisation of two primitives

--- lib/src/primitives/algorithms/halving_doubling.py
+++ lib/src/primitives/algorithms/halving_doubling.py
@@ -1,26 +1,27 @@
-from lib.src.p2p import DataTransferEvent, DataTransferHandler
-from lib.src.primitives.help_functions import max_time
+from lib.src.network_lib.handler.p2p_handler import DataTransferHandler
+from lib.src.network_lib.event.p2p_event import DataTransferEvent
+from lib.src.network_lib.helpers.help_functions import max_time
 
 
 def one_step_in_halving_doubling(handler, event, delta):
-    chunk_size = event.data_size / ( len(event.gpu_cards) * 2 * delta)
+    chunk_size = event.data_size / ( len(event.processors) * 2 * delta)
     halving_doubling_walk(handler, event, chunk_size, delta)
-    applying_time = handler.clock.get_time() + max_time(event.gpu_cards, chunk_size)
+    applying_time = handler.clock.get_time() + max_time(event.processors, chunk_size)
     return applying_time
 
 
 def halving_doubling_walk(handler, event, chunk_size, delta, index=0):
-    if index >= len(event.gpu_cards) // 2:
+    if index >= len(event.processors) // 2:
         return
-    data_handler = DataTransferHandler(handler.future_event_list, handler.clock)
+    data_handler = DataTransferHandler(handler.future_event_list)
     first_index = index
     second_index = index + delta
     handler.future_event_list.add_event(
         DataTransferEvent(handler.clock.get_time(), data_handler,
-                          event.gpu_cards[first_index], event.gpu_cards[second_index], chunk_size)
+                          event.processors[first_index], event.processors[second_index], chunk_size)
     )
     handler.future_event_list.add_event(
         DataTransferEvent(handler.clock.get_time(), data_handler,
-                          event.gpu_cards[second_index], event.gpu_cards[first_index], chunk_size)
+                          event.processors[second_index], event.processors[first_index], chunk_size)
     )
     halving_doubling_walk(handler, event, chunk_size, delta, index + 1)
--- lib/src/primitives/algorithms/ring.py
+++ lib/src/primitives/algorithms/ring.py
@@ -1,14 +1,15 @@
-from lib.src.p2p import DataTransferEvent, DataTransferHandler
-from lib.src.primitives.help_functions import max_time
+from lib.src.network_lib.handler.p2p_handler import DataTransferHandler
+from lib.src.network_lib.event.p2p_event import DataTransferEvent
+from lib.src.network_lib.helpers.help_functions import max_time
 
 
 def one_step_in_ring(handler, event):
     # Assume that the data is divided into equal chunks
-    chunk_size = event.data_size / len(event.gpu_cards)
+    chunk_size = event.data_size / len(event.processors)
     ring_walk(handler, event, chunk_size)
     # If the gpu cards are all the same and have the same bandwidth,
     # the time to apply the event is the same for all of them
-    applying_time = handler.clock.get_time() + max_time(event.gpu_cards, chunk_size)
+    applying_time = handler.clock.get_time() + max_time(event.processors, chunk_size)
     return applying_time
 
 
@@ -18,10 +19,10 @@ def ring_walk(handler, event, data_size, index=0):
     """
     if index >= len(event.gpu_cards):
         return
-    data_handler = DataTransferHandler(handler.future_event_list, handler.clock)
+    data_handler = DataTransferHandler(handler.future_event_list)
     second_index = (index + 1) % len(event.gpu_cards)
     handler.future_event_list.add_event(
         DataTransferEvent(handler.clock.get_time(), data_handler,
-                          event.gpu_cards[index], event.gpu_cards[second_index], data_size)
+                          event.processors[index], event.processors[second_index], data_size)
     )
     ring_walk(handler, event, data_size, index + 1)
