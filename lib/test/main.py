b9b86c112007f70c56f687a1048daead3808b5cf
Max <lonishin.max@gmail.com>
2025-08-04 17:23:59 +0300

    changed the test examle of work and added the funciton which will help to understand the json file

--- lib/test/main.py
+++ lib/test/main.py
@@ -1,23 +1,31 @@
 from lib.src.network_lib.handler.all_gather_handler import AllGatherStepHandler
 from lib.src.network_lib.handler.all_reduce_handler import AllReduceStepHandler
+from lib.src.network_lib.handler.reduce_scatter_handler import ReduceScatterStepHandler
 from lib.src.network_lib.model.processor import Processor
 from lib.src.network_lib.model.network import Network
 from lib.src.network_lib.model.model import Model
 from lib.src.network_lib.utils.methods import Method
-from lib.test.helper import count_events_in_json
+from lib.test.helper import count_events_in_json, describe_event_in_text
 from lib.src.core.engine import Engine
 
 if __name__ == '__main__':
-    filename_stats = "lib/results/complex_test.json"
-    processors = [Processor() for _ in range(2 ** 2)]
-    network = Network(6, 0.2, processors)
+    filename_stats = "lib/results/all_reduce_halving_doubling_results.json"
+    processors = [Processor() for _ in range(2 ** 3)]
+    network = Network(6, 0.0, processors)
     test_engine = Engine()
     test_array = [
-        (AllGatherStepHandler(test_engine.future_event_list, is_start_handler=True), Method.RING, 1600),
-        (AllReduceStepHandler(test_engine.future_event_list, is_start_handler=True), Method.HALVING_DOUBLING, 1600)
+        # (ReduceScatterStepHandler(test_engine.future_event_list, is_start_handler=True), Method.HALVING_DOUBLING, 6400),
+        # (AllGatherStepHandler(test_engine.future_event_list, is_start_handler=True), Method.HALVING_DOUBLING, 6400),
+        (AllReduceStepHandler(test_engine.future_event_list, is_start_handler=True), Method.HALVING_DOUBLING, 6400),
     ]
     model = Model(test_array)
     test_engine.init(filename_stats, model)
     test_engine.execute(network)
     test_engine.save_statistic()
+    describe_event_in_text(filename_stats, network)
+    # step = 4
+    # step_size = 1 << (step - 1)
+    # for me in range(16):
+    #     partner = me ^ step_size
+    #     print(f"{me} -> {partner} (step size: {step_size})")
     # count_events_in_json(filename_stats)
