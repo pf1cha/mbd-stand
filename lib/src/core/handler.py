f9da228d815bedd0d628e58e44afb3d6a143dd12
Max <lonishin.max@gmail.com>
2026-02-19 19:24:31 +0300

    deleted unnecessary class field

--- lib/src/core/handler.py
+++ lib/src/core/handler.py
@@ -2,7 +2,6 @@ class Handler:
     def __init__(self, future_event_list, is_start_handler=False, index=0,
                  next_handler=None):
         self.future_event_list = future_event_list
-        self.index = index
         self.is_start_handler = is_start_handler
         self.next_handler = next_handler  # указатели на следующий(ие) обработчик(и)
         # Maybe delete next_handler because it is not used anywhere
